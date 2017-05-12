dbadb409ac7a770d9fdad74392a19e3de04b5ff0
Valera <yadimov@gmail.com>
2017-05-12 23:33:42 +0300

    prjsct editing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,8 +13,6 @@ $(function () {
    TweenMax.to(into_info, 1, { x: -40, autoAlpha: 1, ease: Power4.easeInOut, delay: 1.6 });
    // TweenMax.to(about_gallery, 2, {autoAlpha: 1, width: '48%',  ease: Power4.easeInOut});
    
-
-
    var tl = new TimelineMax();
    tl
       .fromTo(about_gallery, 1, {autoAlpha: 0, width:0, x: '50%'}, {autoAlpha: 1, width: '48%', x: '0%'})
@@ -23,6 +21,7 @@ $(function () {
 
    Barba.Pjax.start();
    Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container) {
+
       var white_line = $('.white-line');
       var into_h1 = $('.intro__h1-title');
       // var into_bg = $('.intro');
@@ -42,5 +41,38 @@ $(function () {
       .fromTo(timeline, 1, {autoAlpha: 0, y: 40}, {autoAlpha: 1, y: 0}, '-=1');
    });
 
+var Homepage = Barba.BaseView.extend({
+  namespace: 'homepage',
+  onEnter: function() {
+      $("nav ul").removeClass('menu-w');
+      $("nav ul").addClass('menu');
+  },
+  onEnterCompleted: function() {
+   console.log('привет');
+  },
+  onLeave: function() {
+  },
+  onLeaveCompleted: function() {
+  }
+});
+
+Homepage.init();
+
+var Aboutpage = Barba.BaseView.extend({
+  namespace: 'aboutpage',
+  onEnter: function() {
+      $("nav ul").removeClass('menu');
+      $("nav ul").addClass('menu-w');
+  },
+  onEnterCompleted: function() {
+   console.log('привет');
+  },
+  onLeave: function() {
+  },
+  onLeaveCompleted: function() {
+  }
+});
+
+Aboutpage.init();
 
 });
